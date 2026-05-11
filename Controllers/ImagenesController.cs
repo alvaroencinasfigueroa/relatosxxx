d7496b0cc1f3428ac4d18c6fffb8f0bc754b1f5e
AlvaroQQ <alvaro19aef@gmail.com>
2026-05-11 14:29:12 -0400

    Agrega imágenes ambientales aleatorias: controlador, modelo y galería lateral

---
+++ Controllers/ImagenesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Relatosxxx.Data;
+
+namespace Relatosxxx.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagenesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ImagenesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Imagenes/random
+        [HttpGet("random")]
+        public async Task<IActionResult> GetRandomImage()
+        {
+            var imagenes = await _context.ImagenDecorativas
+                .Where(i => i.Activa)
+                .ToListAsync();
+
+            if (imagenes.Count == 0)
+                return NotFound(new { url = "" });
+
+            var random = new Random();
+            var elegida = imagenes[random.Next(imagenes.Count)];
+
+            return Ok(new { url = elegida.Url, categoria = elegida.Categoria });
+        }
+    }
+}
