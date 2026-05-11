ea1d2490763c15790f5edcd4f0c3328752c0accb
AlvaroQQ <alvaro19aef@gmail.com>
2026-05-11 14:44:56 -0400

    Agregar propiedades completas a ImagenDecorativa

--- Models/ImagenDecorativa.cs
+++ Models/ImagenDecorativa.cs
@@ -2,5 +2,11 @@
 {
     public class ImagenDecorativa
     {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Categoria { get; set; }
+        public bool Activa { get; set; } = true;
+
+
     }
 }
